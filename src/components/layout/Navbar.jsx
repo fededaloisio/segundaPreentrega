a1787e963440f95dcf78f01fe948796e4fcc9b11
federico daloisio <fededaloisio1@gmail.com>
2024-02-28 23:51:08 -0300

    Agregamos interacción al logo para volver al inicio

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,21 +1,30 @@
-import CartWidget from "../common/CartWidget"
-import styles from "../../css/Navbar.module.css"
-import { Link } from "react-router-dom"
+import CartWidget from "../common/CartWidget";
+import styles from "../../css/Navbar.module.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className={styles.navbarContainer}>
-        <span>
-            Logo
-        </span>
-        <ul>
-            <Link to="/" ><li>Inicio</li></Link>
-            <Link to="/category/teclado" ><li>Teclados</li></Link>
-            <Link to="/category/mouse" ><li>Mouses</li></Link>
-        </ul>
-        <Link style={{color: "white" , textDecoration:   "none"}} to="/cart"> <CartWidget /> </Link>
+      <Link style={{ color: "white", textDecoration: "none" }} to={"/"}>
+        <span>Logo</span>
+      </Link>
+      <ul>
+        <Link to="/">
+          <li>Inicio</li>
+        </Link>
+        <Link to="/category/teclado">
+          <li>Teclados</li>
+        </Link>
+        <Link to="/category/mouse">
+          <li>Mouses</li>
+        </Link>
+      </ul>
+      <Link style={{ color: "white", textDecoration: "none" }} to="/cart">
+        {" "}
+        <CartWidget />{" "}
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
